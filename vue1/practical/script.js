22ee4c22f1b36dda22049aa68ac49ffe102b6113
VuyaniMkhombo <vuyanimkhombo@gmail.com>
2018-11-07 12:55:09 +0200

    updates

--- vue1/practical/script.js
+++ vue1/practical/script.js
@@ -1,1 +1,19 @@
-console.log("running script")
+var app = new Vue({
+    el: '#app',
+    data:{
+        guess:0,
+        estimate:0
+    },
+    methods: {
+        showAlert: function () {
+            if(app.guess == app.estimate){
+                alert('Your guess is correct ')
+            }
+          
+        }
+      }
+})
+
+app.guess = Math.round(Math.random() * 10)
+
+console.log(app.guess)

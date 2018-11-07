e4d8d338388710e5a1f3bcfd20359466674f522c
VuyaniMkhombo <vuyanimkhombo@gmail.com>
2018-11-07 16:16:14 +0200

    updates

---
+++ Assignment3/sw.js
@@ -0,0 +1,37 @@
+self.importScripts('main.js');
+
+// Files to cache
+var cacheName = 'Assignment3';
+var filesToCache = [
+  '/TransportApp/Assignment3/index.html',
+  '/TransportApp/Assignment3/main.js',
+  '/TransportApp/Assignment3/bulma.mini.css',
+  '/TransportApp/Assignment3/images/Train.jpeg',
+];
+
+// Installing Service Worker
+self.addEventListener('install', function(e) {
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(contentToCache);
+    })
+  );
+});
+
+// Fetching content using Service Worker
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then(function(response) {
+        return caches.open(cacheName).then(function(cache) {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});

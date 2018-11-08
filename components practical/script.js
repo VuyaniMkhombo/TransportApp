5ec1f7a4ac3fa5112d1d063de0fb2bb11db0ca01
VuyaniMkhombo <vuyanimkhombo@gmail.com>
2018-11-08 11:23:43 +0200

    updates

--- components practical/script.js
+++ components practical/script.js
@@ -1,7 +1,33 @@
+function generateGuess(){
+  return Math.round(Math.random() * 10)
+}
+
 const app = new Vue({
   el: "#app",
   data: {
-    timer: 0,
+    timer: 60,
+    computerGuess: generateGuess(),
+    userGues:0,
     errors: [ true, true]
+  },
+  methods: {
+    enter: function(){
+      if(app.computerGuess == app.userGues){
+        alert('Your guess is correct ')
+      }else{
+        alert('Sorry Wrong Guess Try Again!')
+      }
+    }
   }
 })
+
+function countDown() {
+  app.timer--
+  if(app.timer ===0){
+    alert('You failed!')
+    app.timer = 60
+  }
+}
+
+setInterval(countDown, 1000)
+console.log(app.computerGuess)

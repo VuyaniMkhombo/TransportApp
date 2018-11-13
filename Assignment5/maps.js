59bc2a4443364bbc3b2cd1aef2b257a8490287fe
VuyaniMkhombo <vuyanimkhombo@gmail.com>
2018-11-13 17:28:55 +0200

    cha

--- Assignment5/maps.js
+++ Assignment5/maps.js
@@ -1,37 +1,16 @@
 window.onload = function(){
     loadMap();
 }
+function loadMap() {  
+    var mymap = L.map('map').setView([-33.9249,18.4241], 13);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoidXdjbGVjdHVyZXIiLCJhIjoiY2ptdWJ6aWt1MGQ4aDN3bzhiM2V1dnRiYyJ9.lWYq773rwVmRzbyHcYAVHw'
+}).addTo(mymap)
+  
+ 
 
-function searchLocation(){
-    var geo = new MapboxGeocoder({
-        accessToken: mapboxgl.accessToken
-    })
-}
-
-//Load map function
-function loadMap(){
-    //window.location.href = "searchTrain.html";
-    mapboxgl.accessToken = 'pk.eyJ1IjoidGJvLTAiLCJhIjoiY2ptdDhsZXY1MmM3NTNrbnhiOGJ1bHZoaCJ9.qh77ltMqwFPAZrbUcTNNMw';
-    window.map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v10',       
-        center: [18.4241, -33.9249], // starting position [lng, lat]
-        zoom: 9 // starting zoom
-    });
     
-    window.startPin = new mapboxgl.Marker({draggable : true}).setLngLat([0, 0]).addTo(window.map)
-    window.destinationPin = new mapboxgl.Marker({draggable : true}).setLngLat([0, 0]).addTo(window.map)
-    window.map.on('click', function(event){
-        event.preventDefault();
-        if(window.startPoint == true){
-            window.destinationPin.setLngLat(event.lngLat);
-            window.startPoint = false;
-            //document.getElementById('destination').value = event.lngLat.lng + "," + event.lngLat.lat;
-        }else{
-            window.startPin.setLngLat(event.lngLat);
-            window.startPoint = true;
-            //document.getElementById('start').value = event.lngLat.lng + "," + event.lngLat.lat;
-        }
-    })
 }
-document.getElementById('map').appendChild(geocoder.onAdd(map));

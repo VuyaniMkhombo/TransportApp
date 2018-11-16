9b9da5318ced9a1736ff1d9490c4c35364f4857f
VuyaniMkhombo <vuyanimkhombo@gmail.com>
2018-11-16 11:55:50 +0200

    updates

--- Assignment5/script.js
+++ Assignment5/script.js
@@ -6,6 +6,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 const appId = 'UQC0GZBe4uhDMrPh37kB'
 const appCode = 'skrJjuMNRtDPm5YFPW9P4w'
+const clientId = 'f3af64fa-c976-4840-bddd-2e6b4b48160b'
+const clientSecret = 'm38EJQNCAD2VZ1SeQbTtLIIAlhuVjEtUsc7pR8lNLhk='
 
 const autocompleteUrl = "http://autocomplete.geocoder.api.here.com/6.2/suggest.json" +
   "?app_id=" + appId +
@@ -16,6 +18,43 @@ const geocodeUrl = "https://geocoder.api.here.com/6.2/geocode.json" +
   "?app_id=" + appId +
   "&app_code=" + appCode +
   "&searchtext="
+
+const tokenUrl = "https://identity.whereismytransport.com/connect/token"
+
+function login() {
+    var payload = {
+        'client_id': clientId,
+        'client_secret': clientSecret,
+        'grant_type': 'client_credentials',
+        'scope': 'transportapi:all'
+    }
+
+    var request = new XMLHttpRequest();
+    request.open('POST', 'https://identity.whereismytransport.com/connect/token', true);
+    request.addEventListener('load', function () {
+        if(this.status == 200) {
+            var response = JSON.parse(this.responseText);
+            var token = response.access_token;
+            window.token = token;
+
+            localStorage.setItem('token', token)
+            localStorage.setItem('storageDate', Date.now().toLocaleString())
+        } else {
+            console.log("get token call failed")
+            alert('login unsuccessful')
+        }
+    });
+    request.setRequestHeader('Accept', 'application/json');
+    var formData = new FormData();
+
+    for (var key in payload) {
+        formData.append(key, payload[key]);
+    }
+
+    request.send(formData);
+}
+
+login()
 
 var app = new Vue({
     el: '#app',
@@ -38,8 +77,6 @@ var app = new Vue({
                 text = this.destinationAddress
             }
 
-            console.log(text);
-
             if(text.length < 5) {
                 return false
             }
@@ -52,9 +89,6 @@ var app = new Vue({
                     _this.autoCompleteResults = response.suggestions
                     _this.isStart = isStart
                 })
-                .catch(error => {
-                  console.log(error);
-                })
         },
         resultSelect: function (result) {
             var _this = this
@@ -65,11 +99,13 @@ var app = new Vue({
                 .then(function (response) {
                     var location = response.Response.View[0].Result[0].Location.DisplayPosition
                     if(_this.isStart == true) {
+                        _this.startAddress = result.label
                         _this.startPoint = L.marker([location.Latitude, location.Longitude])
                         _this.startPoint.addTo(map)
                         _this.startLocation = location
                         _this.autoCompleteResults = []
                     } else {
+                        _this.destinationAddress = result.label
                         _this.destinationPoint = L.marker([location.Latitude, location.Longitude])
                         _this.destinationPoint.addTo(map)
                         _this.destinationLocation = location
@@ -78,7 +114,52 @@ var app = new Vue({
                 })
         },
         search: function () {
-            // will connect to the whereismytransport api and get some results and do things with it.
+            console.log('running search')
+            var journeyUrl = 'https://platform.whereismytransport.com/api/journeys'
+            var ourBody = {
+                "geometry": {
+                    "type": "MultiPoint",
+                    "coordinates": [
+                        [
+                            this.startLocation.Longitude,
+                            this.startLocation.Latitude
+                        ],
+                        [
+                            this.destinationLocation.Longitude,
+                            this.destinationLocation.Latitude
+                        ]
+                    ]
+                },
+                "maxItineraries": 5
+            }
+
+
+            fetch(journeyUrl, {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + window.token,
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify(ourBody) 
+            })
+            .then(function(response){
+                //console.log(response)
+                return response.json()
+            })
+            .then(function(response) {
+                console.log(response)
+
+                var itineraries = response.itineraries
+                if(itineraries.length > 0) {
+                    var legs = itineraries[0].legs
+                    for(var i = 0; i < legs.length; i++) {
+                        console.log('geometry', legs[i].geometry.coordinates)
+                        var coorindates = legs[i].geometry.coordinates
+
+                    }
+                }
+            })
         }
     }
 })
